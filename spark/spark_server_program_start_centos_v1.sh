1ccf5dd206c92104ab38c94e2ad6b8cdd38d29fc
sam <samuel.alfano@students.ffhs.ch>
2019-02-13 22:44:25 +0100

    added seperate start scripts for spark

--- spark/spark_server_program_start_centos_v1.sh
+++ spark/spark_server_program_start_centos_v1.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Start collectl
-sudo /opt/collectl/collectl-4.3.1/collectl -i1 --all --export graphite,54.83.166.192 &
-# Start master Server
-/opt/spark/spark-2.4.0-bin-hadoop2.7/sbin/start-master.sh &
-/opt/spark/spark-2.4.0-bin-hadoop2.7/sbin/start-slaves.sh &
 # --class = Class with main method
 # --master= Where the master is (standalone mode = localhost)
 # --executor-memory = Max. Memory used by executor
