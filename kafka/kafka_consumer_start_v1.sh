8f870c7360d8f0e85ea016d3f1f73f293f83a56a
sam <samuel.alfano@students.ffhs.ch>
2019-02-21 23:32:43 +0100

    minor flink/kafka changes

--- kafka/kafka_consumer_start_v1.sh
+++ kafka/kafka_consumer_start_v1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo /opt/kafka/kafka_2.11-2.1.0/bin/zookeeper-server-start.sh /opt/kafka/kafka_2.11-2.1.0/config/zookeeper.properties &
 sudo /opt/kafka/kafka_2.11-2.1.0/bin/kafka-server-start.sh /opt/kafka/kafka_2.11-2.1.0/config/server.properties &
-sudo /opt/kafka/kafka_2.11-2.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 24 --topic test &
-#sudo /opt/kafka/kafka_2.11-2.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 24 --topic test-other &
-#sudo /opt/kafka/kafka_2.11-2.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-output &
+sudo /opt/kafka/kafka_2.11-2.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 8 --partitions 24 --topic test &
+sudo /opt/kafka/kafka_2.11-2.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 8 --partitions 24 --topic test-other &
